c29321a70d77a71c5ebe7f302b8033d318a902cd
João Paulo <62178699+joaop2p@users.noreply.github.com>
2022-04-28 00:45:50 -0300

    Add files via upload

---
+++ js/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCJKspeYWROe6459Q3qO05kCEM04A6k_Q0",
+  authDomain: "site-ba253.firebaseapp.com",
+  projectId: "site-ba253",
+  storageBucket: "site-ba253.appspot.com",
+  messagingSenderId: "796498018630",
+  appId: "1:796498018630:web:b426d5daf9d1d1635fe547",
+  measurementId: "G-C3C14ZZ5BW"
+};
